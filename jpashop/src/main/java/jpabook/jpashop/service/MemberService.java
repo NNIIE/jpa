875b01fa311726b0988c2cb52b89d923b8f9687c
Greg.Study <greg0917@naver.com>
2022-05-05 07:33:20 +0900

    회원서비스 (생성자 주입 방식 선택 feat.lombok)

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 읽기전용으로 설정할시 더 나은 성능 최적화를 보여준다 여기선 findMembers(), findOne() 에 적용되었고 join() 메소드에는 @Transactional 어노테이션을 달아서 제외시킴
+@RequiredArgsConstructor // 생성자 주입 (final 이 있는 필드만 가지고 생성자를 만들어줌)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty())
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
