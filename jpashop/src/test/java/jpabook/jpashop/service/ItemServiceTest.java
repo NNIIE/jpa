fe49bf84cc017c9ad114dad2ad6be363fbda5b02
Greg.Study <greg0917@naver.com>
2022-05-06 05:08:30 +0900

    상품 도메인

---
+++ jpashop/src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class ItemServiceTest {
+
+    @Autowired
+    ItemService itemService;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    EntityManager em;
+
+}
